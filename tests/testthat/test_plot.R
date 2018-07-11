4cbaf0307e3f253be979a08bd8f813eb7b207b6e
happma <martinhapp@aon.at>
2018-07-11 23:32:02 +0200

    update unit test

--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -1,7 +1,7 @@
 context("Plot Function")
 
 object_hrm <- HRM::hrm_test(value ~ group*dimension, subject = "subject", data = EEG)
-object_hrm2 <- HRM::hrm_test(value ~ group*dimension, subject = "subject", data = EEG)
+object_hrm2 <- HRM::hrm_test(value ~ dimension, subject = "subject", data = EEG)
 
 
 test_that("function plot", {
@@ -10,7 +10,7 @@ test_that("function plot", {
   expect_equivalent(class(plot(object_hrm)), c("gg", "ggplot"))
   expect_equivalent(class(plot(object_hrm2)), c("gg", "ggplot"))
   expect_equivalent(class(plot(object_hrm2)), c("gg", "ggplot"))
-  expect_equivalent(class(plot(object_hrm, xlab = "time", ylab = "mean", legend = TRUE,
+  expect_equivalent(class(plot(object_hrm, xlab = "time", ylab = "mean", legend = FALSE,
                                legend.title = "", theme_bw() + theme(legend.title = element_blank(), legend.position="none"))
   ), c("gg", "ggplot"))
 })
